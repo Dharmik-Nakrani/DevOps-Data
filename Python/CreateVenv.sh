1271ec4ed85bd4779c2ef86abb293f7339c698d0
Dharmik Nakrani <DharmikN@hotmail.com>
2024-10-10 00:15:53 +0530

    Add Python

---
+++ Python/CreateVenv.sh
@@ -0,0 +1,17 @@
+# Install
+```
+python3.11 -m venv .venv
+```
+# Activate
+```
+source .venv/bin/activate
+```
+# Install Packages
+```
+pip3 install -r requirement.txt
+```
+
+# Deactivate it
+```
+deactivate
+````
